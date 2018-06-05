94d4eaa6dbed8821ff9bbb0cc40039e86d07327b
Borgolov Nikita <nborgolov96@gmail.com>
2018-06-05 22:46:02 +0400

    Update serializer.py

--- serializer.py
+++ serializer.py
@@ -1,56 +1,13 @@
-import requests
-from flask import Flask, request
-import json
-from embeddings import Embeddings
-from loader import Loader
-from serializer import EmbeddingSerializer
+class EmbeddingSerializer:
 
-app = Flask(__name__)
-embedding_model = None
+    def __init__(self, data):
+        self.data = data
+        self.text = ''
+        self.token = ''
 
-
-@app.route('/', methods=['GET'])
-def index():
-    return "Vector service is working"
-
-
-# {
-#     "text": "qwerty",
-#     "token": "asdadasdasdasd"
-# }
-@app.route('/api/embedding', methods=['POST'])
-def embedding():
-    if request.is_json:
-        content = request.get_json()
-        serializer = EmbeddingSerializer(data=content)
-        if not serializer.is_valid():
-            return 'Error'
-        text = serializer.text
-        token = serializer.token
-        vector = Embeddings().build_sentence_vector(text).tolist()
-        data = json.dumps(
-            {
-                "vector": vector,
-                "token": token
-            }
-        )
-        return data
-
-
-
-@app.route('/api/setup', methods=['POST'])
-def set_up():
-    if request.is_json:
-        content = request.get_json()
-        if content['key'] == 'fox':
-            Loader().download_all_models()
-        if content['key'] == 'snake':
-            embedding_model = Embeddings()
-        if content['key'] == 'sitara':
-            Loader().download_all_models()
-            embedding_model = Embeddings()
-        return 'All data is downloaded'
-
-
-if __name__ == '__main__':
-    app.run()
+    def is_valid(self):
+        if self.data:
+            self.text = self.data['text']
+            self.token = self.data['token']
+            return self.text != ''  and self.token != ''
+        return False
